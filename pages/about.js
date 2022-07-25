124bc85eab98124cd0210006ff98f13fa1e7e73f
Faqih Madani <faqihfaqih22@gmail.com>
2022-07-25 16:07:43 +0700

    link save

--- pages/about.js
+++ pages/about.js
@@ -10,7 +10,7 @@ const About = () => {
             <div className="md:pt-28 pt-32 w-11/12 md:w-10/12 lg:w-9/12 xl:w-8/12 mx-auto">
                 <h1 className="text-gray-900 text-3xl text-center font-sans font-medium">About News App</h1>
                 <p className="text-md text-center mt-2">
-                    This website is made by Faqih Madani using Next JS, Tailwind CSS, and <a className="underline" target="_blank" href="https://newsapi.org/"> News API</a>.
+                    This website is made by Faqih Madani using Next JS, Tailwind CSS, and <a className="underline" rel="noreferrer" target="_blank" href="https://newsapi.org/"> News API</a>.
                 </p>
             </div>
             <p>
